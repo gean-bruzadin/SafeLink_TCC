97d0dc6c9837d318d523d933cf63fbcb836aaecd
Gean Carlos <geantokito@gmail.com>
2025-08-19 19:19:15 -0300

    adicionado a conexão com o banco de dados

--- Program.cs
+++ Program.cs
@@ -1,23 +1,47 @@
+using SafeLink_TCC.Config;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuração do DbContext para PostgreSQL
+builder.Services.AddDbContext<DbConfig>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+// Configuração de autenticação com Cookies
+builder.Services.AddAuthentication("CookieAuthentication")
+    .AddCookie("CookieAuthentication", config =>
+    {
+        config.Cookie.Name = "AlunoLoginCookie";
+        config.LoginPath = "/Autenticacao/Login";
+        config.AccessDeniedPath = "/Autenticacao/Login";
+    });
+
+// Adiciona suporte a Controllers e Views
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure o pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Autenticacao}/{action=Login}/{id?}");
 
 app.Run();
