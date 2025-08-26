de1b8fc68041cd81414d2f0b1f9549fcf7b6153c
Gean Carlos <geantokito@gmail.com>
2025-08-26 16:51:39 -0300

    adicionados as imagens para fazer a tela de cadastro e login

---
+++ wwwroot/js/auth.js
@@ -0,0 +1,12 @@
+﻿const cadastroContainer = document.getElementById('cadastro-container');
+const loginContainer = document.getElementById('login-container');
+
+function showLogin() {
+    cadastroContainer.classList.add('hidden');
+    loginContainer.classList.remove('hidden');
+}
+
+function showCadastro() {
+    loginContainer.classList.add('hidden');
+    cadastroContainer.classList.remove('hidden');
+}
