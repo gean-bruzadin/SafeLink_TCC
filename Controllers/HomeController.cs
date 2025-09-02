ac97d8c9527bacf60c4bf9d2ce5859b06b39cff5
gean-bruzadin <geantokito@gmail.com>
2025-09-02 08:38:54 -0300

    feitas algumas correções

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace SafeLink_TCC.Controllers
 {
+    [Authorize] // protege a Home — apenas usuários autenticados acessam.
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            // A View pode ler os claims direto do `User`.
+            return View();
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
